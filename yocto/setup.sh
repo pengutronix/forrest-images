de9da6ee5b1fcf1b90a42e68f308933195c5ee66
Jan Luebbe <jlu@pengutronix.de>
2024-08-19 22:05:06 +0200

    WIP

--- yocto/setup.sh
+++ yocto/setup.sh
@@ -9,5 +9,8 @@ cp "$selfdir/auto.conf" ~/.yocto/
 
 cat "$selfdir/ssh_config" >> ~/.ssh/config
 
+sudo -E apt-get install --assume-yes --no-install-recommends \
+          build-essential chrpath diffstat gawk lz4
+
 sudo mkdir -p /srv/cache
 echo "cache /srv/cache 9p defaults,nofail 0 0" | sudo tee -a /etc/fstab
