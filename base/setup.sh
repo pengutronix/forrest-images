300a2fc110fe33318392a1bf734b78a73a2d3ba7
Leonard Göhrs <l.goehrs@pengutronix.de>
2024-11-19 11:01:00 +0100

    base: apply filesystem speed optimizations
    We can use the fact that most copies of the disk images will be thrown away
    after a single use and that they will only be used again if the machine
    shut down cleanly to optimize the filesystem for speed.
    
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

--- base/setup.sh
+++ base/setup.sh
@@ -6,6 +6,17 @@ self="$(realpath "${0}")" && selfdir="$(dirname "${self}")"
 
 source "${selfdir}/../common/common.sh"
 
+# Most copies of this disk image will not be persisted.
+# Instead they will only live for one build job and will then be deleted.
+# Do not waste time sending discard request in that case.
+# The persist action calls fstrim for disk images that are persisted.
+sudo sed -i "s/,discard,/,nodiscard,/" /etc/fstab
+
+# There is never a case where we have to recover from an unclean shutdown,
+# as the disk image will be thrown away if the machine is stopped.
+# There is thus no need for a filesystem journal.
+sudo tune2fs -O "^has_journal" /dev/vda1
+
 mkdir -p ~/.ssh
 cat "$selfdir/known_hosts" >> ~/.ssh/known_hosts
 
