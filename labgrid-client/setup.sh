b6f63ef3b4ac2ba0c91d72ce7937472cc69436e4
Jan Heinemeyer <j.heinemeyer@pengutronix.de>
2026-06-09 20:48:12 +0200

    labgrid-client: install labgrid globally
    Previously pipx installed packages into the user home directory. This
    requires amending $PATH which was persisted by 'pipx ensurepath' in
    ~/.bashrc.
    
    But GitHub Actions are executed in a non-interactive shell which does
    not read bashrc. If we install the package globally the executables will
    be in '/usr/local/bin/' which is in the default $PATH.
    
    '--include-deps' is needed so pytest and other executables from
    dependencies are available as well.
    
    Signed-off-by: Jan Heinemeyer <j.heinemeyer@pengutronix.de>

--- labgrid-client/setup.sh
+++ labgrid-client/setup.sh
@@ -13,7 +13,7 @@ sudo cp "${selfdir}/20-cuskci.network" /etc/systemd/network/
 sudo -E apt-get install --assume-yes --no-install-recommends \
     pipx
 
-pipx ensurepath
-pipx install git+https://github.com/labgrid-project/labgrid
+sudo -E pipx install --global --include-deps \
+    git+https://github.com/labgrid-project/labgrid
 
 cleanup
