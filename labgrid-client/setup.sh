317db3d9936bba93b0e2232ae66a6569f3459058
Leonard Göhrs <l.goehrs@pengutronix.de>
2026-06-10 13:11:18 +0200

    labgrid-client: configure search domain for the kernelci network
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

--- labgrid-client/setup.sh
+++ labgrid-client/setup.sh
@@ -8,8 +8,7 @@ source "${selfdir}/../common/common.sh"
 
 prepare
 
-sudo cp "${selfdir}/hosts" /etc/hosts
-sudo cp "${selfdir}/20-openlab.network" /etc/systemd/network/
+sudo cp "${selfdir}/20-cuskci.network" /etc/systemd/network/
 
 sudo -E apt-get install --assume-yes --no-install-recommends \
     pipx
